182575823167b39a9bf33b3c85c8c04caa18e297
Berke <zberke97@gmail.com>
2025-08-29 18:18:08 +0300

    feat: Add script to remove all contacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -3,11 +3,10 @@ import { readContacts } from '../utils/readContacts.js';
 export const countContacts = async () => {
   try {
     const allContacts = await readContacts();
-    const countContacts = allContacts.length;
-    console.log('Veritabanındaki kişi sayısı:', countContacts);
+    return allContacts.length;
   } catch (error) {
     console.error('Veritabanındaki kişi sayısı alınırken hata oluştu:', error);
   }
 };
 
-countContacts();
+console.log(await countContacts());
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,13 @@
-export const removeAllContacts = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    await writeContacts([]);
+
+    console.log('İletişim bilgileri veritabanından başarıyla silindi');
+  } catch (error) {
+    console.error('İletişim bilgileri veritabanından silinemedi:', error);
+  }
+};
 
 removeAllContacts();
