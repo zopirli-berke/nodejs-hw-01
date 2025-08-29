b0048e5104816b097d0c801e1037fd59d89a1852
Berke <zberke97@gmail.com>
2025-08-29 17:51:25 +0300

    feat: Add script to generate a single contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,6 +1,20 @@
-import { PATH_DB } from '../constants/contacts.js';
-import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+
+    const newContact = createFakeContact();
+    const updatedContacts = [...existingContacts, newContact];
+
+    await writeContacts(updatedContacts);
+    console.log('Yeni kişi başarıyla eklendi');
+    console.log('Yeni kişi detayları:', newContact);
+  } catch (error) {
+    console.error('Kişi eklenirken hata oluştu:', error);
+  }
+};
 
 addOneContact();
