440d5eeaf74e22ee2d42e704e36b838a926fdb97
Berke <zberke97@gmail.com>
2025-08-29 18:31:38 +0300

    feat: Add script to remove the last contact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,21 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length === 0) {
+      console.log('Veritabanı zaten boş silinecek kişi yok');
+      return;
+    }
+    const removedContact = contacts.pop();
+    console.log('silinen son kişi:', removedContact);
+
+    await writeContacts(contacts);
+    console.log('Veritabanından son kişi başarıyla silindi');
+  } catch (error) {
+    console.log('İşlem sırasında bir hata oluştu:', error);
+  }
+};
 
 removeLastContact();
