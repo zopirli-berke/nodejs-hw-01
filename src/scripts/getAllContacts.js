549e4236f5c05cd8755505bb91e8fa52944155c0
Berke <zberke97@gmail.com>
2025-08-29 17:57:41 +0300

    feat: Add script to get all contacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    console.log('Veritabanındaki tüm kişiler:', contacts);
+  } catch (error) {
+    console.error('Veritabanındaki kişiler alınırken hata oluştu:', error);
+  }
+};
 
 console.log(await getAllContacts());
